045f6a6659f8a86778b6985a257f6da4bce538af
Apuy717 <58309017+Apuy717@users.noreply.github.com>
2019-12-03 23:51:20 +0700

    terbaru pisan

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -36,7 +36,7 @@
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
           <i class="fas fa-fw fa-user"></i>
-          <span>Pages</span>
+          <span>User</span>
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
